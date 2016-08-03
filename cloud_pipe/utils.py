ef24c76c25b4e80b10554eaa65bc0a9c67a5fd80
YX Wang <wangyx2005@gmail.com>
2016-08-03 15:19:57 -0400

    add create_folder()

--- cloud_pipe/utils.py
+++ cloud_pipe/utils.py
@@ -3,6 +3,8 @@ Utility functions for cloud_pipe
 '''
 
 import os.path
+import os
+import errno
 
 
 def get_full_path(path):
@@ -10,3 +12,14 @@ def get_full_path(path):
     get the absolute path.
     '''
     return os.path.abspath(os.path.expanduser(path))
+
+
+def create_folder(folder):
+    '''
+    create folder if not existed
+    '''
+    try:
+        os.makedirs(folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
