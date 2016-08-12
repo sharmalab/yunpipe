749b7a78c0cd23de1b8c083ccf5fc066480781e9
YX Wang <wangyx2005@gmail.com>
2016-08-12 16:19:19 -0400

    change name to yunpipe

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='cloud_pipe',
+setup(name='yunpipe',
       version='0.0.3',
       description='An Automatic tool for setting up your image processing work flow on the cloud',
       url='https://github.com/wangyx2005/cloud_pipe.git',
@@ -12,10 +12,10 @@ setup(name='cloud_pipe',
       # scripts=['bin/wrap', 'bin/setup_pipe'],
       entry_points={
           'console_scripts': [
-              'wrap = cloud_pipe.scripts.wrap:main',
-              'clean-up = cloud_pipe.pipeline.cleanup:main',
-              'setup-pipe = cloud_pipe.scripts.setup_pipe:main',
-              'create-lambda-exec-role = cloud_pipe.scripts.setup_pipe: create_lambda_exec_role']
+              'wrap = yunpipe.scripts.wrap:main',
+              'clean-up = yunpipe.pipeline.cleanup:main',
+              'setup-pipe = yunpipe.scripts.setup_pipe:main',
+              'create-lambda-exec-role = yunpipe.scripts.setup_pipe: create_lambda_exec_role']
       },
       include_package_data=True,
       zip_safe=False)
